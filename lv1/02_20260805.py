c9fb11039f48fd60bab05bcf096c8bdda864a5d0
cchhaaee1023 <chaeyeong5601@gmail.com>
2026-08-06 09:04:35 +0900

    20260805

---
+++ lv1/02_20260805.py
@@ -0,0 +1,7 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/12928
+
+def solution(n):
+    answer = 0
+    li = [i for i in range(1, n+1) if n % i == 0]
+    answer = sum(li)
+    return answer
